5c82f3c70d4ef6bdb810b79fc789e2a678476574
victor hugo rodriguez <al221910532@gmail.com>
2020-11-09 10:15:55 -0600

    migraciones

---
+++ database/migrations/2020_11_09_155700_create_factura_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacturaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('factura', function (Blueprint $table) {
+            $table->integer('numero');
+            $table->date_add('fecha');
+            $table->foreing('id')->references('id')->on('cliente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('factura');
+    }
+}
---
+++ database/migrations/2020_11_09_160511_create_detalle_factura_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetalleFacturaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalle_factura', function (Blueprint $table) {
+            $table->integer('num_detalle');
+            $table->foreing('numero_factura')->references('numero')->on('factura');
+            $table->foreing('id_producto')->references('id')->on('producto');
+            $table->integer('cantidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalle_factura');
+    }
+}
